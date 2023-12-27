37d489fdf7bc56be968ba1fd40811bb72892724f
Francesca Cella <francescacella96@gmail.com>
2023-12-27 17:41:47 +0100

    feat: add card animation

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,7 +1,9 @@
 import { FC } from "react";
-import { Box } from "@chakra-ui/react";
+import { Box, useToken } from "@chakra-ui/react";
 
 import { Card as CardInterface } from "@/store/types";
+
+import "./index.css";
 
 type CardProps = CardInterface & {
   onClick: () => void;
@@ -11,20 +13,37 @@ const Card: FC<CardProps> = ({ content, flipped, disabled, onClick }) => {
   const canShowImage = flipped && !disabled;
   const canBeClicked = !disabled;
 
+  const [gray500] = useToken("colors", ["gray.500"]);
+
   return (
     <Box
-      style={{
-        width: "100px",
-        height: "100px",
-        backgroundImage: canShowImage ? `url(${content})` : undefined,
-        backgroundSize: "contain",
-        backgroundColor: canBeClicked ? "rgba(255,255,255,0.5)" : "transparent",
-        backgroundPosition: "center",
-        backgroundRepeat: "no-repeat",
-        cursor: canBeClicked ? "pointer" : "initial",
-      }}
+      className={["card", flipped ? "card--flipped" : "card"].join(" ")}
+      width="100px"
+      height="100px"
+      cursor={canBeClicked ? "pointer" : "initial"}
       onClick={canBeClicked ? onClick : undefined}
-    />
+      position="relative"
+    >
+      <Box
+        borderRadius="lg"
+        width="100%"
+        height="100%"
+        backgroundColor={canBeClicked ? gray500 : "transparent"}
+        position="absolute"
+      />
+      <Box
+        className="card__inner"
+        borderRadius="lg"
+        width="100%"
+        height="100%"
+        backgroundPosition="center"
+        backgroundRepeat="no-repeat"
+        backgroundSize="contain"
+        backgroundImage={canShowImage ? `url(${content})` : undefined}
+        position="absolute"
+        opacity={flipped ? 1 : 0}
+      />
+    </Box>
   );
 };
 
